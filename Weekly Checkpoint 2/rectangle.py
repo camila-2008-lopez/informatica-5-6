2026e13c14e3ef1224d677fea478f7f67fdd4cf2
camila-2008-lopez <schoolschool32599@gmail.com>
2026-08-17 19:38:46 +0000

    rectangles2

---
+++ Weekly Checkpoint 2/rectangle.py
@@ -0,0 +1,15 @@
+def main():
+    width= int(input("Enter the width of the rectangle: "))
+    print("O" * width)
+    print("O" * width)
+    print("O" * width)
+    print("O" * width)
+    print("O" * width)
+    p = (5* 2) + (width * 2)
+    print("Perimeter:" , p)
+
+
+
+
+if __name__ == "__main__":
+    main()
