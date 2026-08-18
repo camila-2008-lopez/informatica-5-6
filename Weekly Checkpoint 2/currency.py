7ef8f5fbcb46cf06273948e3a02cb7a00c63259d
camila-2008-lopez <schoolschool32599@gmail.com>
2026-08-18 16:37:51 +0000

    currency 2

---
+++ Weekly Checkpoint 2/currency.py
@@ -0,0 +1,6 @@
+def main():
+
+   int(input("What do you have left in pesos?")) = 5600
+   int(input("What do you have left in soles?")) = 105
+   int(input("What do you have left in reais?")) = 280
+print()
