7dc83d682bcc0b27d24ba8c45a1737789e33281e
camila-2008-lopez <schoolschool32599@gmail.com>
2026-08-20 20:12:32 +0000

    qrcode.py 1

---
+++ Weekly Checkpoint 2/qr-code.py
@@ -0,0 +1,15 @@
+import qrcode
+
+def main():
+    song = "https://www.youtube.com/watch?v=QQ6VxXjmCdQ&list=RDQQ6VxXjmCdQ&start_radio=1"
+    qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
+    qr.add_data(song)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="blue" , back_color="white")
+    img.save("my-qrcode.png")
+
+if __name__ == "__main__":
+    main()
+
+
