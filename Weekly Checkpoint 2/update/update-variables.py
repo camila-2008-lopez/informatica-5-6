eddfe32b57f0a24e9c6516b245e27e90e13ef21d
camila-2008-lopez <schoolschool32599@gmail.com>
2026-08-20 16:40:04 +0000

    update variable.py

---
+++ Weekly Checkpoint 2/update/update-variables.py
@@ -0,0 +1,40 @@
+def main():
+    # Augmented Assignment Operator
+    money = 5
+    money = money += 10
+    print(money) # This will print 15
+
+    # Substraction Assignment Operator
+    minutes = 60
+    minutes -= 25
+    print(minutes) # This will print 35
+
+    # Multiply Assignment Operator
+
+    skill = 10
+    skill = *=2
+    print (skill)
+
+    text = "ImVictor"
+    text *= 20
+    print (text)
+
+    # Division Assignment Operator
+
+    pizza = 8
+    people =  int(input("Number of People at the pizza party: "))
+    pizza /= 4
+    print(pizzas)
+
+    # Modulus Assignment Operator
+
+    num = int(input("enter a number: "))
+    divisor = int(input("enter another number: "))
+
+    print(num; "%", divisor,"=", num % divisor)
+
+
+
+    
+if __name__ == "__main__":
+    main()
