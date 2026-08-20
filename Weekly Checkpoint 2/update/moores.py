6b7f27136b7bb42762ef1e6fa87994b853186477
camila-2008-lopez <schoolschool32599@gmail.com>
2026-08-20 17:26:44 +0000

    moores.py 3

--- Weekly Checkpoint 2/update/moores.py
+++ Weekly Checkpoint 2/update/moores.py
@@ -1,7 +1,15 @@
 def main():
     transistors = 17.8
     years = int(input("number of years":))
-    transistors*=2**(years/2)
-    print(transistors)
+    current__year = 2029
+
+if(current__year + years) >= 2030:
+         print("The Law Is Not Valid.")
+
+else:
+    transistors *= 2** (years/2)
+    print(transistors, "Billions")
+
 if __name__ == "__main__":
-    main()
+     main()
+
