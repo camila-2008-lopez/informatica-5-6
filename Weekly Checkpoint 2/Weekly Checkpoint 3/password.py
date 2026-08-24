bab635aba94a4e8372dae61ea741cb54554f1b33
camila-2008-lopez <schoolschool32599@gmail.com>
2026-08-24 19:39:18 +0000

    PASSWORD PY

---
+++ Weekly Checkpoint 2/Weekly Checkpoint 3/password.py
@@ -0,0 +1,16 @@
+def main():
+
+age = int(input("How old are you? "))
+
+if age > 17:
+    print("It's time to go to the temple.")
+
+print("Next")
+
+age = 10
+
+# no colon at the end of the following line
+if age > 17
+
+    print("It's time to go to the temple.")
+
