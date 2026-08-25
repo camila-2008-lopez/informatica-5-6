741f5fb2465de6e9d4fab57744dacb764253326b
camila-2008-lopez <schoolschool32599@gmail.com>
2026-08-25 16:37:26 +0000

    rating.py

---
+++ Weekly Checkpoint 2/Weekly Checkpoint 3/rating.py
@@ -0,0 +1,17 @@
+def main():
+    if 4.5 > 4:
+        print(" 4.5 is greater than 4")
+    elif 4 < 4.5:
+        print("4 is less than 4.5")
+    else:
+        print("4.5 is Perfection)
+
+
+
+
+
+
+
+
+    if __name__ == "__main__":
+        main()
