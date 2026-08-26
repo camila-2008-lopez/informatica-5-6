581861be320d053177f9a1c0286ae95f29e5931d
camila-2008-lopez <schoolschool32599@gmail.com>
2026-08-26 20:15:19 +0000

    if exercise

---
+++ Weekly Checkpoint 2/Weekly Checkpoint 3/if-exercises.py
@@ -0,0 +1,15 @@
+def main():
+    number = int(input("Give me a number: "))
+    if number < 0:
+        number*=-1
+        print(number)
+    else:
+        print(number)
+
+
+        
+
+if __name__ == "__main__":
+    main()
+
+
