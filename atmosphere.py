8ec2b286ae2b182c22e76dd158a043c0d963f727
camila-2008-lopez <schoolschool32599@gmail.com>
2026-08-27 16:38:35 +0000

    atmoshere

---
+++ atmosphere.py
@@ -0,0 +1,23 @@
+def main():
+
+
+
+
+
+
+     2000 m/s  (Exosphere) *  500 m/s  (Thermosphere) +  200 m/s (Mesosphere) + 75 m/s (Stratosphere)
+
+    Exosphere = int(input("700–10,000 km: "))
+    Thermosphere = int(input("85–700 km: "))
+    Mesosphere = int(input("50–85 km: "))
+    Stratosphere = int(input("12–50 km: "))
+    Troposphere = int(input("0–12 km: "))
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
