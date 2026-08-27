c44a75865dafd274bb8025e22fba2645ec462159
camila-2008-lopez <schoolschool32599@gmail.com>
2026-08-27 17:20:59 +0000

    atmosphere

--- atmosphere.py
+++ atmosphere.py
@@ -1,20 +1,22 @@
 def main():
+   Exosphere = int(input("700–10,000 km: "))
+   Thermosphere = int(input("85–700 km: "))
+   Mesosphere = int(input("50–85 km: "))
+   Stratosphere = int(input("12–50 km: "))
+   Troposphere = int(input("0–12 km: "))
 
 
+   Exosphere = (200 = (700/2000))
 
+   Thermosphere = (200 = (85/500))
 
+   Mesosphere = (200 = (50/200))
 
+   Stratosphere = (200 = (12/75))
 
-     2000 m/s  (Exosphere) *  500 m/s  (Thermosphere) +  200 m/s (Mesosphere) + 75 m/s (Stratosphere)
+   Troposphere = (200 = (0/20))
 
-    Exosphere = int(input("700–10,000 km: "))
-    Thermosphere = int(input("85–700 km: "))
-    Mesosphere = int(input("50–85 km: "))
-    Stratosphere = int(input("12–50 km: "))
-    Troposphere = int(input("0–12 km: "))
-
-
-
+   200 = (700/2000)(Exosphere) + 200 = (85/500)(Thermosphere) + 200 = (50/200)(Mesosphere) + 200 = (12/75)( Stratosphere) + 200 = (0/20)(Troposphere)
 
 
 if __name__ == "__main__":
