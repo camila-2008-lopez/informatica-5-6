c00a3156ae142b0a7cf6850b4e4a208ce4450d2f
camila-2008-lopez <schoolschool32599@gmail.com>
2026-09-03 16:28:13 +0000

    roadtrip.py

---
+++ WEEK 4/roadtrip.py
@@ -0,0 +1,16 @@
+def main():
+    answer = "" # Initialize
+
+    while answer != "Yes!": # Condition
+        answer = input("Are we there yet? ").strip().title() # Update
+        if answer == "Yes":
+            followup = input("Really? ").strip().title()
+        if followup == "Yes!":
+            break
+
+
+        print("We just arrived!")
+
+
+if __name__== "__main__":
+    main()
