9218d566eccce085f7d1f26bfc71d6ce2afa5d2d
camila-2008-lopez <schoolschool32599@gmail.com>
2026-09-03 17:28:28 +0000

    guess number.py

---
+++ WEEK 4/guess-number.py
@@ -0,0 +1,14 @@
+import random
+def main():
+
+while answer != "Hello! What is your name?"  # Condition
+        answer = input("Albert").strip().title() # Update
+        if answer == "Well, Albert, I am thinking of a number between 1 and 100.":
+            followup = input("10").strip().title()
+        if followup == "Your guess is too high.":
+            
+
+            break
+
+
+
