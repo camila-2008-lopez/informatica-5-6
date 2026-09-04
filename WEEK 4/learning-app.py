148c066973a13d5e346f1956b4c3e0ebfc8ff3a0
camila-2008-lopez <schoolschool32599@gmail.com>
2026-09-04 17:30:25 +0000

    learning.py

---
+++ WEEK 4/learning-app.py
@@ -0,0 +1,15 @@
+import random
+def main():
+    print("Mathlingo")
+    num1 = random.randint(10,99)
+    num2 = random.randint(10,99)
+    num3 = num1 + num2
+
+    answer = int(input("What is {num1} + {num2}?"))
+    while num3!= 
+
+
+if __name__ == "__main__":
+   main()
+
+
