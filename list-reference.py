399b8c61ba8c3731d0add9fcf53411d8c1186dca
camila-2008-lopez <schoolschool32599@gmail.com>
2026-09-10 20:20:51 +0000

    list

---
+++ list-reference.py
@@ -0,0 +1,11 @@
+def main():
+    fruits("apple","grapes", "orange")
+    fruits.append("banana")
+    print(fruits)
+
+    fruits1 = ["apple", "orange", "grapes"]
+    fruits1.insert(2,"banana")
+    print(fruits1)
+
+    if __name__==__" main__":
+        main()
