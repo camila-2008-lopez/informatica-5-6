0a91719b444864788527f06dc573c1f538293bab
camila-2008-lopez <schoolschool32599@gmail.com>
2026-09-11 17:30:28 +0000

    list

--- list-reference.py
+++ list-reference.py
@@ -1,4 +1,5 @@
 def main():
+    #yellow
     fruits("apple","grapes", "orange")
     fruits.append("banana")
     print(fruits)
@@ -6,6 +7,44 @@ def main():
     fruits1 = ["apple", "orange", "grapes"]
     fruits1.insert(2,"banana")
     print(fruits1)
+
+
+def main():
+    #orange
+    numbers= [1,2,3,4,5,6,7,8,9,10,11]
+    results= max(numbers)
+    print(results)
+    results = sum(numbers)
+    print(results)
+    resultsmin = min(numbers)
+    print(resultsmin)
+
+def main():
+    #red
+    lista =["Rojo","Amarillo","Verde"]
+    print("lista:",lista)
+
+    lista.pop(1)
+    print("con pop(1):",lista)
+    lista.remove("Rojo")
+    print("con remove("Rojo"):",lista)
+
+def main():
+    #Green
+    objects = ["pencil","computer","chair","paper","yoyo","nickel"]
+    objects[len(objects - 1)] = "rock"
+    print(objects)
+    print(len(objects))
+
+    objects.pop(0)
+    print("lista con pop:", objects)
+
+    objects.sort()
+
+
+def main():
+    #Blue
+
 
     if __name__==__" main__":
         main()
