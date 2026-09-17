d86c9fdb0c3a79da20652482bdf28c64677eaa19
camila-2008-lopez <schoolschool32599@gmail.com>
2026-09-17 20:17:01 +0000

    for intro2

--- for-intro.py
+++ for-intro.py
@@ -1,3 +1,18 @@
 def main():
- doctrine =  ["Believe in God", "Repentance", "Receiving holy ghost", "Bauptizim", "marriage"]
- 
+    doctrine =  ["faith", "repentance", "baptism", "confirmation", "endure to the end"]
+    print(doctrine)
+    # for p in words:
+    #     print(word)
+
+height = int(input("height: "))
+for i in range(height):
+print("*"*(i+1))i=0
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
