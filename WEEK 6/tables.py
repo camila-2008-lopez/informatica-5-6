e8e865ff8d8fa99000f6e1ed8450866fbf84e322
camila-2008-lopez <schoolschool32599@gmail.com>
2026-09-23 20:19:41 +0000

    tables.py

---
+++ WEEK 6/tables.py
@@ -0,0 +1,29 @@
+def main():
+Multiply = [1*5,2*5,3*5,4*5,5*5,6*5,7*5,8*5,9*5,10*5]
+
+table = int(input("5: "))
+
+for i in range(5):
+    print("*"*(i*5))
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
